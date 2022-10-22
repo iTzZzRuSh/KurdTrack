0aa8c12693054de142842aca972c7bf193a93b14
Daniel Peshtewan <danielpolen2005@gmail.com>
2022-10-23 01:49:17 +0200

    app

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,7 +1,25 @@
+import { HStack, Icon, IconButton, Link, } from "@chakra-ui/react";
+import { FiHome, FiMessageCircle, FiShoppingBag } from 'react-icons/fi'
+import { SiGooglestreetview } from "react-icons/si";
+
 
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+        <HStack w="full" mt="15px" justifyContent="space-evenly">
+            <Link href="https://www.youtube.com">
+            <IconButton icon={<FiHome/>} aria-label="Home"/>
+            </Link>
+            <Link href="https://www.youtube.com">
+            <IconButton icon={<FiMessageCircle/>} aria-label="Kontakt"/>
+            </Link>
+            <Link href="https://www.youtube.com">
+            <IconButton icon={<FiShoppingBag/>} aria-label="Sklep"/>
+            </Link>
+            <Link href="https://www.youtube.com">
+            <IconButton icon={<SiGooglestreetview/>} aria-label="Adres"/>
+            </Link>
+            
+        </HStack>
   );
 }
